8cc68334d2eb8dc51d44164260fba72ba1d1d9ff
ILeonnard <igorleonnard@gmail.com>
2024-04-16 09:56:15 -0300

    update

--- RNA.js
+++ RNA.js
@@ -1,97 +1,108 @@
 
-// Função número aleatorio
-function randomRange(min, max){
-    return Math.random() * (min - max) + min;
-};
+function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 
-// Função de interpolação linear
-// A interpolação linear é uma forma de adivinhar um valor entre dois números que você já conhece. Imagine esses dois números como pontos em uma linha. Se você quer encontrar um número entre esses dois pontos, a interpolação linear faz isso usando uma fórmula simples. Basicamente, ela "liga os pontos" de forma reta para pegar o valor intermediário. É muito útil quando precisamos de valore entre dois números conhecidos.
+function lerp(a, b, t) {
+  return a + (b - a) * t;
+}
 
-function lerp(a, b ,t) {
-    return a + (b - a) * t;
-};
 
-class Neuron{
-    constructor(inputs) {
-        this.bias = randomRange(-1, 1);
+class Neuron {
+  constructor(inputs) {
+    
+    this.bias = randomRange(-1, 1);
 
-        this.weightList = new Array(inputs)
-        .fill()
-        .map(() => randomRange(-1, 1))
-    }
-};
+    this.weightList = new Array(inputs)
+      .fill()
+      .map(() => randomRange(-1, 1));
+  };
 
-g(signalList = []); {
+  g(signalList = []) {
     let u = 0;
-    
-    for(let i = 0;i<this.weightList.lenght;i++){
-        u += signalList[i] * this.weightList[i]
+
+    for (let i = 0; i < this.weightList.length; i++) {
+      u += signalList[i] * this.weightList[i];
     }
 
-    if (Math.tanh(u) > this.bias) return 1; // Ativado
-    else return 0; // Desativado
-};
-    
-mutate(rate = 1); {
-    this.weightList * this.weightList.map(() => {
-        return lerp(w, randomRange(-1, 1), rate)
-    });
 
-    this.bias = lerp(this.bias, randomRange(-1, 1), range);
-};
+    if (Math.tanh(u) > this.bias) return 1; 
+    else return 0;
+  }
+
+
+  mutate(rate = 1) {
+    this.weightList = this.weightList.map((w) => {
+
+      return lerp(w, randomRange(-1, 1), rate);
+    });
+   
+    this.bias = lerp(this.bias, randomRange(-1, 1), rate);
+  }
+}
+
 
 class RNA {
-    constructor(inputCount = 1, levelList = []) {
-        this.score = 0;
+  constructor(inputCount = 1, levelList = []) {
 
-        this.levelList = levelList.map((l, i) => {
-            const inputSize = i === 0 ? inputCount : levelList[i - 1]
+    this.score = 0;
 
-            return new Array(l).fill().map(() => new Neuron(inputSize));
-        });
+
+    this.levelList = levelList.map((l, i) => {
+
+      const inputSize = i === 0 ? inputCount : levelList[i - 1];
+
+
+      return new Array(l).fill().map(() => new Neuron(inputSize));
+    });
+  }
+
+  
+
+
+  compute(list = []) {
+    for (let i = 0; i < this.levelList.length; i++) {
+      const tempList = [];
+
+      for (const neuron of this.levelList[i]) {
+        if (list.length !== neuron.weightList.length) throw new Error('Entrada inválida');
+        tempList.push(neuron.g(list));
+      }
+      list = tempList; 
     }
+    return list;
+  }
 
-    compute(list = []) {
-        for(let i = 0;i<this.levelList.length;i++){
-            const tempList = []
 
-            for (const neuron of this.levelList[i]) {
-                if (list.length !== neuron.weightList.length) throw new Error("Entrada invalida");
-                tempList.push(neuron.g(list))
-            }
-            list = tempList
-        }
-        return list;
+  mutate(rate = 1) {
+    for (const level of this.levelList) {
+      for (const neuron of level) neuron.mutate(rate);
     }
-}
-
-mutate(rate =  1); {
-    for (const level of this.levelListf) {
-        for(const neuron of level) neuron.mutate(rate)
-    }
-}
+  }
 
 
-load(rna); {
+  load(rna) {
     if (!rna) return;
     try {
-        this.levelList = rna.map((neuronList) => {
-            return neuronList.map((neuron) => {
-                const n = new Neuron();
-                n.bias = neuron.bias
-                n.weightList = neuron.weightList;
+      this.levelList = rna.map((neuronList) => {
 
-                return n;
-            });
+        return neuronList.map((neuron) => {
+
+          const n = new Neuron();
+          n.bias = neuron.bias;
+          n.weightList = neuron.weightList;
+          return n;
         });
-    } catch(e) {
-        return;
+      });
+    } catch (e) {
+      return;
     }
+  }
 
-    save(); {
-        return this.levelList;
-    }
+  save() {
+    return this.levelList;
+  }
 }
 
 export default RNA;
--- script.js
+++ script.js
@@ -1,89 +1,88 @@
-import utils from "./utils";
-import RNA from "./RNA";
-import controls from "./controls";
+import utils from './utils.js';
+import RNA from './RNA.js';
+import controls from './controls.js';
 
-const SAMPLES = 20
+const SAMPLES = 20;
 const game = Runner.instance_;
-let dinoList = []
-let dinoIndex = 0
+let dinoList = [];
+let dinoIndex = 0;
 
-let bestScore = 0;
-let bestRNA = nul;
+let bestScore = 0; 
+let bestRNA = null; 
 
-function fillDinoList() {
-    for(let i=0;i<SAMPLES;i++) {
-        dinoList[i] = new RNA(3, [10, 10, 2])
-        dinoList[i].load(bestRNA)
-        if (i > 0) dinoList[i].mutate(0.5)
-    }
-
-    console.log('Lista de dinossauros criada!')
+function fillDinoList () {
+  for (let i=0; i<SAMPLES; i++) {
+    dinoList[i] = new RNA(3, [10, 10, 2]); 
+    dinoList[i].load(bestRNA); 
+    if (i > 0) dinoList[i].mutate(0.5); 
+  }
+  console.log('Dino list created!');
 }
 
 setTimeout(() => {
-    fillDinoList()
-    controls.dispatch('jump')
-}, 1000)
+  fillDinoList();
+  controls.dispatch('jump'); 
+}, 1000);
 
 setInterval(() => {
-    if(!game.activated) return 
+  if (!game.activated) return; 
 
-    const dino = dinoList(dinoIndex)
+  const dino = dinoList[dinoIndex];
 
-    if (game.crashed) {
-        if (dino.score > bestScore) {
-            bestScore = dino.score
-            bestRNA = dino.save()
-            console.log('Melhor pontuacao: ', bestScore)
-        }
-        dinoIndex++
-    
-
-    if (dinoIndex === SAMPLES) {
-        fillDinoList();
-        dinoIndex = 0
-        bestScore = 0
+  if (game.crashed) { 
+    if (dino.score > bestScore) {
+      bestScore = dino.score;
+      bestRNA = dino.save(); 
+      console.log('Melhor ponduacao:', bestScore);
     }
-    game.restart()
+    dinoIndex++;
+
+    if (dinoIndex === SAMPLES) { 
+      fillDinoList();
+      dinoIndex = 0;
+      bestScore = 0;
+    }
+    game.restart();
   }
 
-  const {tRex, horizon, currentSpeed, distanceRan, dimansions} = game
-  dino.score = distanceRan - 2000
+  const { tRex, horizon, currentSpeed, distanceRan, dimensions } = game;
+  dino.score = distanceRan - 2000;
 
   const player = {
     x: tRex.xPos,
     y: tRex.yPos,
-    speed: currentSpeed
+    speed: currentSpeed,
   };
 
   const [obstacle] = horizon.obstacles
     .map((obstacle) => {
       return {
         x: obstacle.xPos,
-        y: obstacle.yPos
+        y: obstacle.yPos,
       }
     })
     .filter((obstacle) => obstacle.x > player.x)
 
-    if (obstacle) {
-        const distance = 1 - (utils.getDistance(player, obstacle) / dimansions.WIDTH);
-        const speed = player.speed / 6
-        const heigh = Math.tanh(105 - obstacle.y)
+  if (obstacle) { 
+    const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
+    const speed = player.speed / 6;
+    const height = Math.tanh(105 - obstacle.y); 
 
-        const [jump, crouch] = dino.compute([
-            distance, 
-            speed,
-            heigh,
-        ]);
+   
+    const [jump, crounch] = dino.compute([
+      distance,
+      speed,
+      height,
+    ]);
 
-        if (jump === crouch) return;
-        if (jump) controls.dispatch('jump') 
-        if (crouch) controls.dispatch('crouch')
-    };
+  
+    if (jump === crounch) return;
+    if (jump) controls.dispatch('jump');
+    if (crounch) controls.dispatch('crounch');
+  }
 }, 100);
-
 
 /* const s = document.createElement('script');
 s.type = 'module';
-s.src = 'https://localhost:5500/script.js'
+s.src = 'http://localhost:5500/script.js';
 document.body.appendChild(s); */
